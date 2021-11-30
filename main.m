2a9901485ad8cd0cec8b61247a4af55afbc7a21e
SasakiSaki <galaster@foxmail.com>
2021-11-30 13:17:38 +0800

    🎀 Support Unix Shell

--- main.m
+++ main.m
@@ -10,9 +10,36 @@ $commitDateString = \"``\"
 $env:GIT_COMMITTER_DATE = $commitDateString
 git commit --amend --no-edit --date $commitDateString
 git rebase --continue
-$env:GIT_COMMITTER_DATE = \"\"
 ", {DateString[FromUnixTime[unix], "ISODateTime"]}
 ];
-powershell = StringRiffle[log /@ all, "\n"];
+last="
+$env:GIT_COMMITTER_DATE =\"\"
+";
+powershell = StringRiffle[log /@ all, "\n"]<>last;
 powershell // CopyToClipboard
 Export["time-travel.ps1", powershell, "Text"]
+
+
+
+
+(* ::Package:: *)
+(* git rebase -i --root *)
+SetDirectory@NotebookDirectory[];
+all = Sort@RandomInteger[UnixTime /@ {
+    DateObject[{2021,7, 27}], 
+    DateObject[{2021,8, 10}]
+}, 19];
+log[unix_] := TemplateApply["\
+commitDateString=\"``\"
+GIT_COMMITTER_DATE=$commitDateString git commit --amend --no-edit --date $commitDateString
+git rebase --continue
+", {DateString[FromUnixTime[unix], "ISODateTime"]}
+];
+last="
+GIT_COMMITTER_DATE=\"\"
+";
+
+powershell = StringRiffle[log /@ all, "\n"]<>last;
+
+powershell // CopyToClipboard
+Export["time-travel.sh", powershell, "Text"]

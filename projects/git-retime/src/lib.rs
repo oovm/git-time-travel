f3da54fed8a5d8f9b0e24e7bc2abaf996edf03c2
Aster <aster@vers.site>
2022-09-03 19:17:37 +0000

    📝 Add detailed documents

--- projects/git-retime/src/lib.rs
+++ projects/git-retime/src/lib.rs
@@ -3,16 +3,18 @@ use clap::Parser;
 mod rebase;
 mod utils;
 
+/// Configs for `git-retime`
 #[derive(Parser)]
-#[command(author, version, about, long_about = None)]
+#[command(author, version, about, long_about = include_str!("../readme.md"))]
 pub struct GitTimeTravel {
     /// commit hash
     pub commit: String,
     /// start date
     pub start_date: String,
-    /// end date
+    /// end date, default is `start date + commit count`
     #[arg(short, long, value_name = "END")]
     pub end_date: Option<String>,
+    /// custom branch name, default is `time-travel`
     #[arg(short, long, value_name = "BRANCH")]
     pub branch: Option<String>,
 }
